e81e5a268e5d074b9a14fbb4be6fdb8661e15982
AnkitSeal2023 <ankitseal2021@gmail.com>
2026-01-22 16:41:42 +0530

    feat: backend logging complete

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ import StepProgress from "../components/progressBar";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black px-2">
-      <nav className="theme-switcher fixed top-0 right-0 z-50">
+    <div className="relative flex min-h-screen items-center justify-center bg-background px-2">
+      <nav className="fixed top-4 right-4 z-50">
         <ThemeToggle />
       </nav>
       <StepProgress />
